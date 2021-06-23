8deefb812c5ec541bb9508297e0d1f2edcfbf564
Philip Kearney <philip.kearney@personio.de>
2021-06-23 13:13:43 +0100

    Updated next version to 11

---
+++ next1/components/exposedTitle.js
@@ -0,0 +1,16 @@
+import React from "react";
+const ExportredTitle = () => {
+  console.log("---------loading remote component---------");
+
+  return (
+    <div className="hero">
+      <h1 className="title">
+        {" "}
+        This came fom <code>next1</code> !!!
+      </h1>
+      <p className="description">And it works like a charm v2</p>
+    </div>
+  );
+};
+
+export default ExportredTitle;
